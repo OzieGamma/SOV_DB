3c964afacbf801c3220ced446e3b2a37926f41b6
Oswald Maskens <ozie@live.be>
2015-03-24 15:12:19 +0100

    added simple queries

---
+++ Queries.sql
@@ -0,0 +1,12 @@
+﻿
+-- 2.e
+SELECT Gender, MIN(Height) FROM dbo.Person GROUP BY Gender
+GO
+
+SELECT Gender, MAX(Height) FROM dbo.Person GROUP BY Gender --WHERE Gender = 'F'
+GO
+
+SELECT Gender, AVG(Height) FROM dbo.Person GROUP BY Gender --WHERE Gender = 'F'
+GO
+
+SELECT TOP(10) * FROM dbo.Person where Gender = 'F'
