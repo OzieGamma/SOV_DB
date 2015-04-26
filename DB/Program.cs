a31cfbb5fa0bec1672275a9e99c82db0ef864272
Solal Pirelli <solal.pirelli@gmail.com>
2015-04-26 15:05:53 +0200

    Cleaner import.

--- DB/Program.cs
+++ DB/Program.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using DB.Parsing;
 
 namespace DB
 {
-    using System.Text;
-    using System.Threading.Tasks;
-
     public static class Program
     {
-        // Debug messages will be printed every time this number of lines is parsed
-        private const int ReportPeriod = 100000;
-
         // All CSV files must be in that folder, without renaming them
         private const string InputFilesPath =
             //@"C:\Users\Oswald\Downloads\Movies"; // Oswald
@@ -22,89 +12,8 @@ namespace DB
         private static void Main()
         {
             Console.WriteLine( "Doing work..." );
-            ConvertCsvs();
+            DatabaseImport.ImportFromDirectory( InputFilesPath );
             Console.Read();
-        }
-
-        private static async void ConvertCsvs()
-        {
-            var parsers = new ILineParser[] {
-                new AlternativePersonNameParser(),
-                new AlternativeProductionTitleParser(),
-                new CharacterParser(),
-                new CompanyParser(),
-                new PersonParser(),
-                new ProductionParser(),
-                new ProductionCastParser(),
-                new ProductionCompanyParser(),
-            };
-
-            var tempDir = new DirectoryInfo( Path.Combine( Path.GetTempPath(), "DB_CSV" ) );
-            if ( tempDir.Exists )
-            {
-                tempDir.Delete( true );
-            }
-            tempDir.Refresh();
-            tempDir.Create();
-
-            Parallel.ForEach( parsers, parser =>
-            {
-                var results = from obj in ParseCsv( parser )
-                              group obj by obj.GetType() into grouped
-                              select new { Name = grouped.Key.Name, Items = from o in grouped select o.ToString() };
-
-                foreach ( var result in results )
-                {
-                    var path = Path.Combine( tempDir.FullName, result.Name );
-                    File.WriteAllLines( path, result.Items, new UnicodeEncoding( false, true ) );
-                }
-            } );
-
-            await Database.DisableReferentialIntegrityAsync();
-            await Database.DropAllAsync();
-            await Database.CreateAllAsync();
-            foreach ( var file in tempDir.EnumerateFiles() )
-            {
-                string command = string.Format( "BULK INSERT {0} FROM '{1}' WITH ( CODEPAGE = 1200, DATAFILETYPE = 'widechar' )", file.Name, file.FullName );
-                await Database.ExecuteAsync( command );
-            }
-
-            Console.WriteLine( "Done." );
-        }
-
-        private static IEnumerable<object> ParseCsv( ILineParser parser )
-        {
-            int lineNumber = 0;
-            var builder = new StringBuilder();
-            foreach ( var values in ReadCsv( parser.FileName ) )
-            {
-                foreach ( var value in parser.Parse( values ) )
-                {
-                    yield return value;
-                }
-
-                lineNumber++;
-                if ( lineNumber % ReportPeriod == 0 )
-                {
-                    Console.WriteLine( "[{0}] Done with {1}.", parser.FileName, lineNumber );
-                }
-            }
-        }
-
-        private static IEnumerable<string[]> ReadCsv( string fileName )
-        {
-            var path = Path.Combine( InputFilesPath, fileName.ToUpper() + ".csv" );
-
-            using ( TextReader reader = new StreamReader( File.OpenRead( path ) ) )
-            {
-                string line = reader.ReadLine();
-
-                while ( line != null )
-                {
-                    yield return line.Split( '\t' ).Select( val => val.Trim() ).ToArray();
-                    line = reader.ReadLine();
-                }
-            }
         }
     }
 }
