bf32752bb179281509ecc5ad739e0474c1ce428e
Solal Pirelli <solal.pirelli@gmail.com>
2015-05-03 12:29:29 +0200

    Yorick!

---
+++ DBGui/Sql/KnownStatements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DBGui.Sql
+{
+    public static class KnownStatements
+    {
+        public static Statement[] Part2
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public static Statement[] Part3
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ DBGui/Sql/Statement.cs
@@ -0,0 +1,16 @@
+﻿namespace DBGui.Sql
+{
+    public sealed class Statement
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Command { get; private set; }
+
+        public Statement( string name, string description, string command )
+        {
+            Name = name;
+            Description = description;
+            Command = command;
+        }
+    }
+}
